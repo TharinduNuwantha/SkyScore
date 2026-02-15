fce5c2d0427ab70a519ff64695597534ef2c6378
Tharindu Nuwantha <105153249+TharinduNuwantha@users.noreply.github.com>
2026-02-15 16:54:33 +0530

    configure server.js

--- weather-client/src/App.jsx
+++ weather-client/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Dashboard from './components/Dashboard/Dashboard'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Navbar/>
+      <Dashboard/>
     </>
   )
 }
