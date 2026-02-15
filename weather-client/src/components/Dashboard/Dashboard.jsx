f5ceb0a9a0ee0c17f84bfe82c92f3bbcda9b6851
Tharindu Nuwantha <105153249+TharinduNuwantha@users.noreply.github.com>
2026-02-15 19:51:13 +0530

    data fetching

--- weather-client/src/components/Dashboard/Dashboard.jsx
+++ weather-client/src/components/Dashboard/Dashboard.jsx
@@ -1,13 +1,33 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Dashboard = () => {
-    const [city,setCity] = useState([]);
+    const [citys,setCitys] = useState([]);
     const [loarding,setLoarding] = useState(true);
+
+useEffect(()=>{
+    fetch("http://localhost:5000/api/weather")
+    .then((res)=>res.json())
+    .then((data) =>{
+        setCitys(data);
+        setLoarding(false);
+    })
+    .catch((err)=>console.log(err));
+});
+
+  if (loarding) return <div>Loading...</div>;
+
   return (
-    <div>
-      {}
+    <div className="dashboard-container">
+      {citys.map((city) => (
+        <div key={city.name} className="city-card">
+          <h2>{city.rank}. {city.name}</h2>
+          <p>Weather: {city.weatherDescription}</p>
+          <p>Temperature: {city.temperature} °C</p>
+          <p>Comfort Score: {city.comfortScore}</p>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
 export default Dashboard
